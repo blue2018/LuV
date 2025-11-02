54c6659d49ff46bf0f8d890df0e26a44430635a3
blue2018 <13889354+blue2018@users.noreply.github.com>
2025-11-02 12:24:31 +0800

    Add new customer site APIs to customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,16 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    "api_1": {
+            "name": "TV-1080资源",
+            "api": "https://api.1080zyku.com/inc/api_mac10.php",
+            "detail": "https://api.1080zyku.com"
+    },
+    "api_2": {
+            "name": "AV-155资源",
+            "api": "https://155api.com/api.php/provide/vod",
+            "detail": "https://155api.com"
     }
 };
 
